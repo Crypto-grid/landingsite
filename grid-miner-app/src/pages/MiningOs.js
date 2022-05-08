8d52013266ddaa4a6ede16198e5903d274cdb91d
Ian Shaw <ianshaw93@gmail.com>
2022-05-08 12:09:34 +0100

    initial commit

---
+++ grid-miner-app/src/pages/MiningOs.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavComponent from '../components/Navbar';
+
+
+export default function MiningOs() {
+    return(
+        <>
+            <NavComponent/>
+            MiningOS
+        </>
+
+    )
+}
